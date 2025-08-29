a7c3aeead503e6e200d9cb64dfd5d301ae5564a1
Aleksa Rodic <ar20231086@student.fon.bg.ac.rs>
2025-08-29 15:58:11 +0200

    thread title modifikacija migration

--- database/migrations/2025_08_29_114626_create_threads_table.php
+++ database/migrations/2025_08_29_114626_create_threads_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('threads', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('title');
         });
     }
 
