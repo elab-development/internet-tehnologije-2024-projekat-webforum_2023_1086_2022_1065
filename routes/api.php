b95c29f1633a3f68ad2a16bebdfd2ee0d5aaa475
Aleksa Rodic <ar20231086@student.fon.bg.ac.rs>
2025-08-31 21:58:49 +0200

    popravljene i testirane API rute

--- routes/api.php
+++ routes/api.php
@@ -13,14 +13,26 @@ use App\Http\Controllers\CategoryController;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
+// thread search TESTIRANO (NOTE: mora da bude u ovom php fajlu ispred resursne rute!!!)  
+Route::get('/threads/search', [ThreadController::class, 'search']);
+
+// resursne rute za citanje TESTIRANO
 Route::apiResource('users', UserController::class)->only(['index', 'show']);
 Route::apiResource('threads', ThreadController::class)->only(['index', 'show']);
 Route::apiResource('comments', CommentController::class)->only(['index', 'show']);
 
+// resursne rute za upis i update TESTIRANO
+Route::middleware('auth:sanctum')->group(function () {
+    //Route::apiResource('users', UserController::class)->only(['store', 'update']); // nije nuzno za sad
+    Route::apiResource('threads', ThreadController::class)->only(['store', 'update']);
+    Route::apiResource('comments', CommentController::class)->only(['store', 'update']);
+});
+
+// TESTIRANO
 Route::post('register', [AuthController::class, 'register']);  // registracija
 Route::post('login', [AuthController::class, 'login']);        // login
 
-// logout ruta
+// logout ruta TESTIRANO
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']); // logout
     Route::get('user', function(Request $request) {
@@ -28,7 +40,7 @@ Route::middleware('auth:sanctum')->group(function () {
     });
 });
 
-// ruta za reset lozinke
+// ruta za reset lozinke TESTIRANO
 Route::middleware('auth:sanctum')->post('/reset-password', [AuthController::class, 'resetPassword']);
 
 // delete korisnika – samo admin
@@ -41,9 +53,7 @@ Route::delete('/threads/{thread}', [ThreadController::class, 'destroy'])
     ->middleware(['auth:sanctum', 'can:manage-threads'])
     ->name('threads.destroy');
 
-// kategorije
+// kategorije TESTIRANO
 Route::apiResource('categories', CategoryController::class)
     ->only(['index', 'show']);
 
-// thread search    
-Route::get('/threads/search', [ThreadController::class, 'search']);
