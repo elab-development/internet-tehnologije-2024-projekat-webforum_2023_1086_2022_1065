03b25067d842cdac4a1b234e52ecfed54f93ce61
Aleksa Rodic <ar20231086@student.fon.bg.ac.rs>
2025-09-03 18:45:25 +0200

    dodato rutiranje u react aplikaciju

--- frontend/web-forum/src/App.js
+++ frontend/web-forum/src/App.js
@@ -2,15 +2,19 @@ import logo from './logo.svg';
 //import './App.css';
 //import './style.css';
 import New from './pages/New'
-//import LoginForm from './pages/LoginForm';
+import LoginForm from './pages/LoginForm';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // rutiranje biblioteka
 
 function App() {
   return (
-    <div>
+    
+    <Router>
+      <Routes>
+        <Route path="/" element={<New />} />
+        <Route path="/login" element={<LoginForm />} />
+      </Routes>
+    </Router>
 
-      <New />
-
-    </div>
   );
 }
 
