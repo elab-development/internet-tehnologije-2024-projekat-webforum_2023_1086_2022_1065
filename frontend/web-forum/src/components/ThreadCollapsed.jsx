a4a32efb58f379075f062c9a63c94fe969e059e5
Aleksa Rodic <ar20231086@student.fon.bg.ac.rs>
2025-09-04 15:54:20 +0200

    dodatne informacije o threadu sada na ui

--- frontend/web-forum/src/components/ThreadCollapsed.jsx
+++ frontend/web-forum/src/components/ThreadCollapsed.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 
-const Thread = ({ id, title, body, status, user, category}) => { // u zagradi je dictionary propova (property valjda???)
+const Thread = ({ id, title, body, status, user, category_id, created_at }) => {
+  // array kategorija po id-u
+  const categories = ["Društvo", "Zabava", "Tehnologija", "Obrazovanje", "Igrice"];
+  const categoryName = ("["+categories[category_id - 1]+"]") || "";
+
+
+  // formatiranje datuma
+  const formattedDate = new Date(created_at).toLocaleString("sr-RS", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
   return (
     <div className="post">
       <h3 className="imekorisnika">{user.username}</h3>
-      <h3>{title}</h3>
-      <p id="posttekst">
-        {body}
-      </p>
+      <p id="thread-time">{formattedDate}</p>
+      <h3>
+        <p id="thread-category">{categoryName}</p>
+        {title}
+      </h3>
+      <p id="posttekst">{body}</p>
       <input type="text" placeholder="Vaš komentar..." />
       <button type="submit">Postavi</button>
     </div>
